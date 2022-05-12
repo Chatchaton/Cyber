7677d867cffd2d771809e003bdf7aa7f1a125724
lubin <lubin07000@gmail.com>
2022-05-13 00:30:36 +0200

    update WOObin

--- src/Menu.java
+++ src/Menu.java
@@ -1,32 +1,69 @@
-import jdk.jfr.Description;
-
+import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Menu {
 
-    private int choice = 0;
+    public enum MenuOption { // hidden options only after visible ones //
+        SIGN,
+        VALIDATE,
+        GEN_PAIR,
+        DEFAULT
+    }
+    final private String menuOptionNames[] = new String[]{ // menu option names for enums //
+            "sign file",
+            "validate file",
+            "generate key pair"
+    };
+
+
+    private MenuOption choice = MenuOption.DEFAULT;
     final private Scanner scanner = new Scanner(System.in);
+
+
 
     public Menu() {}
 
-    @Description("prints menu")
-    public void printMenu() {
-        System.out.println("---------------");
-        System.out.println("1 - sign");
-        System.out.println("2 - validate");
-        System.out.println("---------------");
+
+    public void printMenu()
+    {
+        System.out.println("------------------------");
+        for(int i=0; i<menuOptionNames.length; i++)
+        {
+            System.out.println(i + " - " + menuOptionNames[i]);
+        }
+        System.out.println("------------------------");
+        System.out.print("> ");
     }
+
+
     /**
-     @return choice ID
+     prints menu -> gets user input -> returns option enum
+     @return selected option as: < MenuOption > enum
     */
-    @Description("prints menu and returns choice ID")
-    public int operateMenu() { // print menu and return choice //
-        printMenu();
-        this.choice = scanner.nextInt();
+    public MenuOption operateMenu() // prints menu and return choice //
+    {
+        boolean validChoice = false;
+        int input = 0;
+
+        while(validChoice == false)
+        {
+            printMenu();
+            input = scanner.nextInt();
+            if(input >= 0 && input < menuOptionNames.length)
+            {
+                validChoice = true;
+            }
+            else
+            {
+                System.out.println("- value not supported -");
+            }
+        }
+        this.choice = MenuOption.values()[input];
         return this.choice;
     }
 
-    public void setChoice(int choice) { this.choice = choice; }
-    public int getChoice() { return this.choice; }
+    public void setChoice(MenuOption choice) { this.choice = choice; }
+    public MenuOption getChoice() { return this.choice; }
 
 }
