444f076f8a75a8fa2a0150d30fcb737625c44df0
lubin <lubin07000@gmail.com>
2022-05-12 22:11:16 +0200

    test6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         System.out.println("Hello world! My people");
         System.out.println("Hello world! My people");
+
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
+        keyPairGen.initialize(2048);
+        KeyPair pair = keyPairGen.generateKeyPair();
     }
 }
