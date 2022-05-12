fd79a48e6c176ae78e209742904f457ded5bc268
lubin <lubin07000@gmail.com>
2022-05-12 22:31:06 +0200

    update WOObin

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String args[]) throws Exception {
 
         Main main = new Main();
+        main.printMenu();
 
         //Creating KeyPair generator object
         KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
