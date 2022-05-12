786a5b28d2cd775d246fab168b93dcad6d23182f
lubin <lubin07000@gmail.com>
2022-05-12 22:40:05 +0200

    update WOObin

--- src/Menu.java
+++ src/Menu.java
@@ -3,8 +3,8 @@ public class Menu {
     public Menu() {}
     public void printMenu() {
         System.out.println("---------------");
-        System.out.println("sign - 1");
-        System.out.println("validate - 2");
+        System.out.println("1 - sign");
+        System.out.println("2 - validate");
         System.out.println("---------------");
     }
 
